c0dbdae12b7b96ecef88a21d29b7926a3ca937fe
jeffbroadhurst18 <jeffbroadhurst18@outlook.com>
2018-05-05 20:34:36 +0100

    Add project files.

---
+++ SamuraiAppCore.Data/SamuraiContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using SamuraiAppCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiAppCore.Data
+{
+	public class SamuraiContext : DbContext
+	{
+		private IConfiguration _config;
+
+		public DbSet<Samurai> Samurais { get; set; }
+		public DbSet<Battle> Battles { get; set; }
+		public DbSet<Quote> Quotes { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.Entity<SamuraiBattle>().HasKey(s => new { s.BattleId, s.SamuraiId });
+			base.OnModelCreating(builder);
+
+		}
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+			_config = new ConfigurationBuilder()
+				.AddJsonFile("appsettings.json", true, true)
+				.Build();
+
+			optionsBuilder.UseSqlServer("Server=BROADHURST_WIN8\\BROADHURST;Database=SamuraiDataCore;Trusted_Connection=True;MultipleActiveResultSets=true;");
+		}
+	}
+}
---
+++ SamuraiAppCore.Domain/Quote.cs
@@ -0,0 +1,10 @@
+﻿namespace SamuraiAppCore.Domain
+{
+	public class Quote
+	{
+		public int Id { get; set; }
+		public string Text { get; set; }
+		public Samurai Samurai { get; set; }
+		public int SamuraiId { get; set; } //add this field for it to be used as foreign key
+	}
+}
